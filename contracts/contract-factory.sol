483959b784a9d4f4a7be2a4bbee6c7b885b72129
Hans Hüppelshäuser <hans.hueppelshaeuser@icloud.com>
2023-08-02 15:19:41 +0200

    Adding matching contract

--- contracts/contract-factory.sol
+++ contracts/contract-factory.sol
@@ -2,19 +2,48 @@
 pragma solidity ^0.8.0;
 
 import "./contract.sol";
+import "./matching.sol";  // Assuming both contracts are in the same directory
 
 contract ContractFactory {
+
+    MatchingService private matchingServiceInstance;
+    address[] public registeredContracts;
+
+    constructor(address _matchingServiceAddress) {
+        matchingServiceInstance = MatchingService(_matchingServiceAddress);
+
+        // Set this contract as the factory address in the MatchingService contract
+        matchingServiceInstance.setFactoryAddress(address(this));
+    }
+
+
     mapping(address => Contract[]) public userContracts;
     event ContractCreated(address indexed user, Contract newContract);
 
-    function createContract( uint256 _amount) public payable {
+
+    function registerNewContract(address _contractAddress) external {
+        // Call the registerContract() function on the MatchingService contract
+        matchingServiceInstance.registerContract(_contractAddress);
+
+        // Optionally, store the registered contract's address in this factory for record-keeping
+        registeredContracts.push(_contractAddress);
+    }
+
+
+    function createContract(uint256 _amount) public payable {
         require(msg.value == _amount, "Sent value does not match the specified amount.");
         Contract newContract = new Contract{value: _amount}(msg.sender);
         userContracts[msg.sender].push(newContract);
+
+        // Call registerNewContract with the new contract's address
+        this.registerNewContract(address(newContract));
+
         emit ContractCreated(msg.sender, newContract);
     }
+
 
     function getContractsByUser(address user) public view returns (Contract[] memory) {
         return userContracts[user];
     }
+
 }
---
+++ contracts/matching.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MatchingService {
+
+    struct MatchingServiceObject {
+        string name;
+        uint256 matches;
+        uint256 requests;
+    }
+
+    MatchingServiceObject[5] public services;
+
+    address public FACTORY_ADDRESS;
+    bool public isFactoryAddressSet = false;
+
+    mapping(address => bool) public registeredContracts;
+    address[] public registeredContractsList; // Added this line
+
+    // Declare the event
+    event LowestMatchService(string serviceName, uint256 serviceRating);
+
+    modifier onlyFactory() {
+        require(msg.sender == FACTORY_ADDRESS, "Only the factory can call this");
+        _;
+    }
+
+    modifier onlyRegisteredContracts() {
+        require(registeredContracts[msg.sender], "Only registered contracts can call this");
+        _;
+    }
+
+    constructor() {
+        services[0] = MatchingServiceObject("ms1", 10, 15);
+        services[1] = MatchingServiceObject("ms2", 15, 20);
+        services[2] = MatchingServiceObject("ms3", 20, 30);
+        services[3] = MatchingServiceObject("ms4", 5, 10);
+        services[4] = MatchingServiceObject("ms5", 8, 12);
+    }
+
+    function setFactoryAddress(address _factoryAddress) external {
+        require(!isFactoryAddressSet, "Factory address is already set");
+        FACTORY_ADDRESS = _factoryAddress;
+        isFactoryAddressSet = true;
+    }
+
+    function registerContract(address contractAddress) external onlyFactory {
+        require(!registeredContracts[contractAddress], "Contract is already registered"); // Additional check to prevent duplicate addresses
+        registeredContracts[contractAddress] = true;
+        registeredContractsList.push(contractAddress); // Added this line
+    }
+
+    function getAllRegisteredContracts() external view returns (address[] memory) {
+        return registeredContractsList;
+    }
+
+    function getMatchingService(string[] memory names) public {
+        uint256 lowestMatches = type(uint256).max;
+
+        string memory lowestMatchServiceName = "";
+        uint256 lowestMatchServiceRating;
+
+        for (uint i = 0; i < names.length; i++) {
+            for (uint j = 0; j < services.length; j++) {
+                if (keccak256(bytes(services[j].name)) == keccak256(bytes(names[i]))) {
+                    if (services[j].matches < lowestMatches) {
+                        lowestMatches = services[j].matches;
+                        lowestMatchServiceName = services[j].name;
+                        lowestMatchServiceRating = (services[j].matches * 100) / services[j].requests; // Multiply by 100 for two decimal places
+                        services[j].requests += 1;
+                    }
+                }
+            }
+        }
+        // Emit the event with the result
+        emit LowestMatchService(lowestMatchServiceName, lowestMatchServiceRating);
+    }
+
+    function addMatch(string memory serviceName) external onlyRegisteredContracts {
+        for (uint i = 0; i < services.length; i++) {
+            if (keccak256(bytes(services[i].name)) == keccak256(bytes(serviceName))) {
+                services[i].matches += 1;
+            }
+        }
+    }
+}
