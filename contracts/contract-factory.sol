e98822355253869471c3b2dd5c9fba184b99253f
Hans Hüppelshäuser <Hans.Hueppelshaeuser@ibm.com>
2023-08-11 18:46:04 +0200

    Adding Counter

--- contracts/contract-factory.sol
+++ contracts/contract-factory.sol
@@ -9,6 +9,11 @@ contract ContractFactory {
     MatchingService private matchingServiceInstance;
     address[] public registeredContracts;
 
+    uint256 public contractCounter = 0;  // Counter to keep track of contract IDs
+
+    // Mapping from contract ID to contract address
+    mapping(uint256 => address) public contractsByID;
+
     constructor(address _matchingServiceAddress) {
         matchingServiceInstance = MatchingService(_matchingServiceAddress);
 
@@ -18,8 +23,7 @@ contract ContractFactory {
 
 
     mapping(address => Contract[]) public userContracts;
-    event ContractCreated(address indexed user, Contract newContract);
-
+    event ContractCreated(address indexed user, Contract newContract, uint256 contractID);  // Added contractID to the event
 
     function registerNewContract(address _contractAddress) external {
         // Call the registerContract() function on the MatchingService contract
@@ -28,22 +32,28 @@ contract ContractFactory {
         // Optionally, store the registered contract's address in this factory for record-keeping
         registeredContracts.push(_contractAddress);
     }
-
+    
 
     function createContract(uint256 _amount) public payable {
         require(msg.value == _amount, "Sent value does not match the specified amount.");
         Contract newContract = new Contract{value: _amount}(msg.sender);
         userContracts[msg.sender].push(newContract);
 
+        // Increment contract counter and map new contract's address to the counter
+        contractCounter++;
+        contractsByID[contractCounter] = address(newContract);
+
         // Call registerNewContract with the new contract's address
         this.registerNewContract(address(newContract));
 
-        emit ContractCreated(msg.sender, newContract);
+        emit ContractCreated(msg.sender, newContract, contractCounter);
     }
-
 
     function getContractsByUser(address user) public view returns (Contract[] memory) {
         return userContracts[user];
     }
 
+    function getContractByID(uint256 contractID) public view returns (address) {
+        return contractsByID[contractID];
+    }
 }
